4118c3f4dad2f9ef16a69b2505898340db27c473
Sarath C Ani <mailsforsarath98@gmail.com>
2020-03-29 02:53:56 +0530

    Bug Fixed auth

--- client/src/Router/Login.js
+++ client/src/Router/Login.js
@@ -2,11 +2,16 @@ import React from 'react'
 import { connect } from 'react-redux';
 import { Redirect } from 'react-router-dom'
 import SignIn from '../components/Login/Login'
+import { Spinner } from 'reactstrap';
 
 function Login(props) {
     return (
         <div>
-            {props.isAuthenticated ? <Redirect to='/' /> : <SignIn />}
+            {props.isAuthenticated === false ? <SignIn /> : props.isAuthenticated === true ? <Redirect to='/' /> : (
+            <center>
+                <Spinner />
+            </center>
+            ) }
         </div>
     )
 }
--- client/src/Router/SignUp.js
+++ client/src/Router/SignUp.js
@@ -2,12 +2,17 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { Redirect } from 'react-router-dom';
 import Register from '../components/Register/Register';
+import {Spinner} from 'reactstrap'
 
 function SignUp(props) {
 
     return (
         <div>
-            {(props.isAuthenticated === true)? (<Redirect to='/' />) : (<Register />)}
+            {props.isAuthenticated === false ? <Register /> : props.isAuthenticated === true ? <Redirect to='/' /> : (
+            <center>
+                <Spinner />
+            </center>
+            ) }
         </div>
     )
 }
